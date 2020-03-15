5c0245737945ba91926512f71fb321a7c63adb9a
Freedyool <miracle_ccc@outlook.com>
2020-03-15 19:44:29 +0800

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -4,17 +4,12 @@
 #include <cmath>
 #define MAX 9999
 
-// int maps(char *map,int n,char a);
-// int minimum(int *t,int n);
+int maps(char *map,int n,char a);
+int minimum(int *t,int n);
+int string_to_int(string s);
 
-// void _init(int** graph, char* map,int n);
-// void add_edges(int** graph, char* map,int n);
-// void Dijkstra(int** graph, char* map,int n);
-// void Floyd(int** graph, char* map,int n);
-// void Prim(int** graph, char* map,int n);
-
-// extern void _init(int** graph, char* map,int n);
-// extern void add_edges(int** graph, char* map,int n);
-// extern void Dijkstra(int** graph, char* map,int n);
-// extern void Floyd(int** graph, char* map,int n);
-// extern void Prim(int** graph, char* map,int n);
+void _init(int** graph, char* map,int n);
+void add_edges(int** graph, char* map,int n);
+void Dijkstra(int** graph, char* map,int n);
+void Floyd(int** graph, char* map,int n);
+void Prim(int** graph, char* map,int n);
