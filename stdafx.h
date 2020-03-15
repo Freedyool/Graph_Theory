96cd5475d0afc24b81b24ed241820616f2d01827
Freedyool <miracle_ccc@outlook.com>
2020-03-15 19:43:41 +0800

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cmath>
+#define MAX 9999
+
+// int maps(char *map,int n,char a);
+// int minimum(int *t,int n);
+
+// void _init(int** graph, char* map,int n);
+// void add_edges(int** graph, char* map,int n);
+// void Dijkstra(int** graph, char* map,int n);
+// void Floyd(int** graph, char* map,int n);
+// void Prim(int** graph, char* map,int n);
+
+// extern void _init(int** graph, char* map,int n);
+// extern void add_edges(int** graph, char* map,int n);
+// extern void Dijkstra(int** graph, char* map,int n);
+// extern void Floyd(int** graph, char* map,int n);
+// extern void Prim(int** graph, char* map,int n);
