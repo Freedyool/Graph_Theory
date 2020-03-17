e2ec5e840b1a615adf1a280035a22ffa08385f20
Freedyool <miracle_ccc@outlook.com>
2020-03-18 00:16:14 +0800

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -1,15 +1,8 @@
 #pragma once
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <cmath>
 #define MAX 9999
 
 int maps(char *map,int n,char a);
 int minimum(int *t,int n);
-int string_to_int(string s);
-
-void _init(int** graph, char* map,int n);
-void add_edges(int** graph, char* map,int n);
-void Dijkstra(int** graph, char* map,int n);
-void Floyd(int** graph, char* map,int n);
-void Prim(int** graph, char* map,int n);
